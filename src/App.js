7efef2ddc6c94f5ae50570191b0fa1ab5dc4c4e6
xdays <easedays@gmail.com>
2020-05-05 16:35:05 +0800

    change to xdays life

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Countdown from "react-countdown-now";
+import { IconContext } from "react-icons";
+import { RiHomeHeartLine } from "react-icons/ri";
+import "./App.css";
+
+// Random component
+const Completionist = () => <span>You are good to go!</span>;
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {days} Days {hours} Hours:{minutes} Minutes:{seconds} Seconds
+      </span>
+    );
+  }
+};
 
 class App extends Component {
   render() {
+    const end = Date.parse("2020-05-30T00:00:00");
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <IconContext.Provider value={{ size: "3em" }}>
+            <div>
+              <RiHomeHeartLine />
+            </div>
+          </IconContext.Provider>
+          <h1>What if life is not of xdays ?</h1>
+          <Countdown date={end} renderer={renderer} />
         </header>
       </div>
     );
